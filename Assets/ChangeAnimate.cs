6e756e51a81605977f1fea4235ac76a788a60294
6204101317 <cddvd123455v8@gmail.com>
2022-02-25 21:58:51 +0700

    6204101317

---
+++ Assets/ChangeAnimate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+  public void getAnimatorRun(){
+         this.GetComponent<Animator>().Play("Jog In Circle");
+    }
+    public void getgetAnimatorJum(){
+        this.GetComponent<Animator>().Play("Jump Atta;ck");
+    }
+}
